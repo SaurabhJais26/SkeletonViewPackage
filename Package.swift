2f01be90cd14c4699cc449dc52b9f80d05c25afd
Saurabh Jaiswal <sjsaurabhjais@gmail.com>
2025-04-17 17:17:18 +0530

    v1.0.1

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "SkeletonViewPackage",
     platforms: [
-        .iOS(.v13),
+        .iOS(.v15),
         .macOS(.v13)
     ],
     products: [
